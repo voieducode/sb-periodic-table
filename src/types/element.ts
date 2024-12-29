24857839429891ca0d61a36b85d3412238a6aa4c
Dinh Doan Van Bien <999472+voieducode@users.noreply.github.com>
2024-12-29 19:49:29 +0000

    add missing element fields

--- src/types/element.ts
+++ src/types/element.ts
@@ -3,9 +3,12 @@ export interface Element {
   appearance: string | null;
   atomic_mass: number;
   boil: number | null;
+  bohr_model_image: string | null;
+  bohr_model_3d: string | null;
+  spectral_img: string | null;
   category: string;
   density: number | null;
-  discovered_by: string;
+  discovered_by: string | null;
   melt: number | null;
   molar_heat: number | null;
   named_by: string | null;
@@ -18,12 +21,16 @@ export interface Element {
   symbol: string;
   xpos: number;
   ypos: number;
+  wxpos:  number;
+  wypos:  number;
   shells: number[];
   electron_configuration: string;
   electron_affinity: number | null;
   electronegativity_pauling: number | null;
+  electron_configuration_semantic: string | null;
   ionization_energies: number[];
   block: string;
+  cpk_hex: string | null;
   image: {
     title: string;
     url: string;
